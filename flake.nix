53a3cbab7297ff64abf9ef9e4cf3efa2a8dc3af6
rice-cracker-dev <ricecracker2234@proton.me>
2025-03-25 20:00:33 +0700

    flake: updated inputs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nvf.url = "github:NotAShelf/nvf/a5dee946a9eb749649d17a9d4cd78271600247d0";
+    nvf.url = "github:NotAShelf/nvf";
   };
 
   outputs = {
