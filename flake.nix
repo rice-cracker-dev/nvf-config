a2619ae994bbadb2427814a11dc2385b6a918956
rice-cracker-dev <ricecracker2234@proton.me>
2025-04-22 14:41:21 +0700

    nvf: switch to upstream

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nvf.url = "github:rice-cracker-dev/nvf/lint-required-files";
+    nvf.url = "github:NotAShelf/nvf";
   };
 
   outputs = {
