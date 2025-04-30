29eadec0475ad37f2addd16587aeb1c3c268927e
rice-cracker-dev <ricecracker2234@proton.me>
2025-04-30 16:51:53 +0700

    flake: support more systems

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nvf.url = "github:NotAShelf/nvf";
-    flake-utils = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
@@ -10,17 +10,17 @@
     flake-utils,
     nvf,
     ...
-  }: flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = nixpkgs.legacyPackages.${system};
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
 
-    configModule = import ./configuration.nix;
+      configModule = import ./configuration.nix;
 
-    customNeovim = nvf.lib.neovimConfiguration {
-      modules = [configModule];
-      inherit pkgs;
-    };
-  in {
-    # this will make the package available as a flake input
-    packages.${system}.default = customNeovim.neovim;
-  });
+      customNeovim = nvf.lib.neovimConfiguration {
+        modules = [configModule];
+        inherit pkgs;
+      };
+    in {
+      packages.default = customNeovim.neovim;
+    });
 }
