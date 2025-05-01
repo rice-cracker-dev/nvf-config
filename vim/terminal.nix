b15a131e32bebcdb6967325b528a695242c74659
rice-cracker-dev <ricecracker2234@proton.me>
2025-05-01 20:52:56 +0700

    keymap: remapped <C-\><C-n> to <esc>

--- vim/terminal.nix
+++ vim/terminal.nix
@@ -1,10 +1,20 @@
 {
-  config.vim.terminal.toggleterm = {
-    enable = true;
-    lazygit.enable = true;
-    setupOpts = {
-      direction = "float";
-      open_mapping = ["<ESC>"];
+  config.vim = {
+    terminal.toggleterm = {
+      enable = true;
+      lazygit.enable = true;
+      setupOpts = {
+        direction = "float";
+      };
     };
+
+    keymaps = [
+      {
+        key = "<esc>";
+        mode = "t";
+        action = "<C-\\><C-n>";
+        noremap = false;
+      }
+    ];
   };
 }
