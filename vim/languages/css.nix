5ac4634cb0c878f8145c57e7d374b5f99d406dda
rice-cracker-dev <ricecracker2234@proton.me>
2025-08-09 00:34:24 +0700

    languages/css: disable cssls in tailwindcss projects

--- vim/languages/css.nix
+++ vim/languages/css.nix
@@ -9,14 +9,38 @@ in {
     treesitter.grammars = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
       css
       scss
-      less
-      sass
     ];
 
-    lsp.servers.tailwindcss = {
-      capabilities = mkLuaInline "capabilities";
-      on_attach = mkLuaInline "default_on_attach";
-      cmd = ["${pkgs.tailwindcss-language-server}/bin/tailwindcss-language-server" "--stdio"];
+    lsp.servers = {
+      cssls = {
+        capabilities = mkLuaInline "capabilities";
+        on_attach = mkLuaInline "default_on_attach";
+        cmd = ["${pkgs.vscode-css-languageserver}/bin/vscode-css-languageserver" "--stdio"];
+        root_dir =
+          mkLuaInline
+          # lua
+          ''
+            function(bufnr, on_dir)
+              local packagejson = vim.fs.find("package.json", { type = "file", upward = true })[1]
+
+              if packagejson ~= nil then
+                for line in io.lines(packagejson) do
+                  if line:find("tailwindcss") then
+                    return
+                  end
+                end
+              end
+
+              on_dir(vim.fn.getcwd())
+            end
+          '';
+      };
+
+      tailwindcss = {
+        capabilities = mkLuaInline "capabilities";
+        on_attach = mkLuaInline "default_on_attach";
+        cmd = ["${pkgs.tailwindcss-language-server}/bin/tailwindcss-language-server" "--stdio"];
+      };
     };
   };
 }
