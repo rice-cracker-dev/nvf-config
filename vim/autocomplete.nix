a58094c87d80b0dbf5f4fa88a8382ff155b165e7
rice-cracker-dev <ricecracker2234@proton.me>
2025-08-20 19:42:00 +0700

    autocomplete: prioritize lazydev

--- vim/autocomplete.nix
+++ vim/autocomplete.nix
@@ -1,5 +1,23 @@
 {
   config.vim.autocomplete.blink-cmp = {
     enable = true;
+
+    setupOpts.sources = {
+      default = [
+        "lazydev"
+        "lsp"
+        "path"
+        "snippets"
+        "buffer"
+      ];
+
+      providers = {
+        lazydev = {
+          name = "LazyDev";
+          module = "lazydev.integrations.blink";
+          score_offset = 100;
+        };
+      };
+    };
   };
 }
