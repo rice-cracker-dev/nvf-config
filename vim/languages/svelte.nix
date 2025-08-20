c8d0b316f1b5f32d63410897b3b12532c6844499
rice-cracker-dev <ricecracker2234@proton.me>
2025-08-20 17:06:13 +0700

    languages/svelte: trigger '$/onDidChangeTsOrJsFile' event on .ts or .js change

--- vim/languages/svelte.nix
+++ vim/languages/svelte.nix
@@ -12,8 +12,31 @@ in {
 
     lsp.servers.svelte = {
       capabilities = mkLuaInline "capabilities";
-      on_attach = mkLuaInline "default_on_attach";
       cmd = ["${pkgs.svelte-language-server}/bin/svelteserver" "--stdio"];
+      on_attach = mkLuaInline ''
+        function(client, bufnr)
+          -- Workaround to trigger reloading JS/TS files
+          -- See https://github.com/sveltejs/language-tools/issues/2008
+          vim.api.nvim_create_autocmd('BufWritePost', {
+            pattern = { '*.js', '*.ts' },
+            group = vim.api.nvim_create_augroup('svelte_js_ts_file_watch', {}),
+            callback = function(ctx)
+              -- internal API to sync changes that have not yet been saved to the file system
+              client:notify('$/onDidChangeTsOrJsFile', { uri = ctx.match })
+            end,
+          })
+
+          vim.api.nvim_buf_create_user_command(bufnr, 'LspMigrateToSvelte5', function()
+            client:exec_cmd({
+              title = 'Migrate Component to Svelte 5 Syntax',
+              command = 'migrate_to_svelte_5',
+              arguments = { vim.uri_from_bufnr(bufnr) },
+            })
+          end, { desc = 'Migrate Component to Svelte 5 Syntax' })
+
+          default_on_attach(client, bufnr)
+        end
+      '';
     };
   };
 }
