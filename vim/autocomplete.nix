6ab5a04991d1d03f6907e5050393b2fefa3bda7b
rice-cracker-dev <ricecracker2234@proton.me>
2025-08-21 21:15:57 +0700

    autocomplete: tweak blink settings

--- vim/autocomplete.nix
+++ vim/autocomplete.nix
@@ -1,23 +1,77 @@
 {lib, ...}: let
   inherit (lib) mkForce;
+  inherit (lib.generators) mkLuaInline;
 in {
   config.vim.autocomplete.blink-cmp = {
     enable = true;
 
-    setupOpts.sources = {
-      default = mkForce [
-        "lazydev"
-        "lsp"
-        "path"
-        "snippets"
-        "buffer"
-      ];
+    setupOpts = {
+      appearance = {
+        nerd_font_variant = "normal";
 
-      providers = {
-        lazydev = {
-          name = "LazyDev";
-          module = "lazydev.integrations.blink";
-          score_offset = 100;
+        kind_icons = {
+          Text = "";
+          Method = "";
+          Function = "";
+          Constructor = "";
+
+          Field = "";
+          Variable = "";
+          Property = "";
+
+          Class = "";
+          Interface = "";
+          Struct = "";
+          Module = "󰅩";
+
+          Unit = "";
+          Value = "";
+          Enum = "";
+          EnumMember = "";
+
+          Keyword = "";
+          Constant = "";
+
+          Snippet = "";
+          Color = "";
+          File = "";
+          Reference = "";
+          Folder = "";
+          Event = "";
+          Operator = "";
+          TypeParameter = "";
+        };
+      };
+
+      completion = {
+        list.selection = {
+          preselect = false;
+          auto_insert = false;
+          show_on_insert = true;
+          show_on_backspace = true;
+          show_on_backspace_in_keyword = true;
+        };
+
+        menu.draw.columns = mkLuaInline ''
+          { { 'kind_icon', 'kind', gap = 1 }, { 'label', 'label_description', gap = 1 } }
+        '';
+      };
+
+      sources = {
+        default = mkForce [
+          "lazydev"
+          "lsp"
+          "path"
+          "snippets"
+          "buffer"
+        ];
+
+        providers = {
+          lazydev = {
+            name = "LazyDev";
+            module = "lazydev.integrations.blink";
+            score_offset = 100;
+          };
         };
       };
     };
