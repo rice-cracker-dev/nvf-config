3758d7387d49a216519e6f5279e5ffbbe9a18683
rice-cracker-dev <ricecracker2234@proton.me>
2025-10-27 11:56:59 +0700

    therock: init

--- vim/therock.nix
+++ vim/therock.nix
@@ -6,17 +6,33 @@
   inherit (lib.generators) mkLuaInline;
   inherit (lib.nvim.lua) toLuaObject;
 in {
-  config.vim.overseer.templates = [
-    {
-      name = "vine_boom";
-      builder = mkLuaInline ''
-        function()
-          return {
-            cmd = {${toLuaObject "${pkgs.ffmpeg}/bin/ffplay"}},
-            args = {${toLuaObject ../assets/vine-boom.mp3}},
-          }
-        end
-      '';
-    }
-  ];
+  config.vim = {
+    overseer.templates = [
+      {
+        name = "vine_boom";
+        builder = mkLuaInline ''
+          function()
+            return {
+              cmd = {${toLuaObject "${pkgs.ffmpeg}/bin/ffplay"}},
+              args = {${toLuaObject ../assets/vine-boom.mp3}, "-autoexit", "-nodisp"},
+            }
+          end
+        '';
+      }
+    ];
+
+    autocmds = [
+      {
+        event = ["DiagnosticChanged"];
+        callback = mkLuaInline ''
+          function(args)
+            local diagnostics = vim.diagnostic.get(args.buf, { severity = vim.diagnostic.severity.ERROR })
+            if #diagnostics > 0 and vim.api.nvim_get_mode().mode == "n" then
+              require("overseer").run_template({ name = "vine_boom" })
+            end
+          end
+        '';
+      }
+    ];
+  };
 }
