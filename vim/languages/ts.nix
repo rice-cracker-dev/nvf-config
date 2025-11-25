be9fd1c8530026ba84a32edd335071e35a382121
rice-cracker-dev <ricecracker2234@proton.me>
2025-11-25 23:38:08 +0700

    Revert "languages/ts: reload routegen file on every route changes"
    This reverts commit c80e7403fc7760f2fd6399049b794890a7e3b888.

--- vim/languages/ts.nix
+++ vim/languages/ts.nix
@@ -19,16 +19,5 @@ in {
       cmd = ["${pkgs.typescript-language-server}/bin/typescript-language-server" "--stdio"];
       settings.preferences.quotePreference = "single";
     };
-
-    luaConfigRC.tanstack_router_auto_reload = ''
-      local routegen_path = vim.fs.joinpath(vim.fn.getcwd(), "src", "routeTree.gen.ts")
-
-      if vim.uv.fs_stat(routegen_path) then
-        vim.api.nvim_create_autocmd({'BufWritePost'}, {
-          pattern = "*/src/routes/**",
-          command = "silent! edit! src/routeTree.gen.ts",
-        })
-      end
-    '';
   };
 }
