af2ae8c8ab393bbd8fe2255c658af95cd576d7c7
rice-cracker-dev <ricecracker2234@proton.me>
2025-12-21 00:23:18 +0700

    plugins/lazydev: init

--- plugins/default.nix
+++ plugins/default.nix
@@ -4,6 +4,7 @@
     ./copilot.nix # haha vibe coding goes brrrr
     ./direnv.nix
     ./html-ts-autotag.nix
+    ./lazydev.nix
     ./nvim-dap-python.nix
     ./overseer.nix
     ./schema-store.nix
---
+++ plugins/lazydev.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}: {
+  vim.lazy.plugins.${pkgs.vimPlugins.lazydev-nvim.pname} = {
+    package = pkgs.vimPlugins.lazydev-nvim;
+    ft = "lua";
+    setupModule = "lazydev";
+    setupOpts = {
+      library = [
+        {
+          path = "\${3rd}/luv/library";
+          words = ["vim%.uv"];
+        }
+      ];
+    };
+  };
+}
